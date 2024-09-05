50aa888d8b2a6160c9f6a30b74e52917833617a8
axellgadiel <gadielaxell@gmail.com>
2024-09-05 17:27:18 +0700

    getting started crud backend

--- individualProject/src/components/Navbar.jsx
+++ individualProject/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
         <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
           {/* Left Side (Logo/Title) */}
           <div className="flex items-center space-x-2">
-            <div className="text-xl font-bold">Text to image</div>
+            <div className="text-xl font-bold">Imaginate</div>
             <span className="text-gray-400">/\</span>
             <div className="text-xl font-bold text-black"></div>
           </div>
